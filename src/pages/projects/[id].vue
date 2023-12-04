d421d773c1bcdfa145dc9221c7e2273a4b3325c9
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-12-04 00:41:12 -0500

    structure page one project ok

--- src/pages/projects/[id].vue
+++ src/pages/projects/[id].vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { pb } from '@/backend'
 import { ProjectId } from '@/backend';
+import ProjectContent from '@/components/ProjectContent.vue';
 import TheProjectAnim from '@/components/TheProjectAnim.vue';
+import CardProject from '@/components/CardProject.vue';
 import { useHead } from '@unhead/vue'
 const props = defineProps<{
     id: string,
@@ -11,6 +13,9 @@ const props = defineProps<{
 const ProjectInfo = await ProjectId(props.id);
 const img0 = props.image
 const urlImg0 = img0 && pb.getFileUrl(props, img0, { thumb: '100x200' })
+import { allProjectsSorted } from '@/backend';
+const ProjectsListeSorted = await allProjectsSorted();
+console.log(ProjectsListeSorted)
 
 useHead({
     title: `${ProjectInfo.title} | Lisa Cingolani`
@@ -18,10 +23,21 @@ useHead({
 </script>
 
 <template>
-    <TheProjectAnim/>
-    <h1>{{ ProjectInfo.title }}</h1>
-    <img :src="urlImg0"/>
-    <p>{{ ProjectInfo.description }}</p>
+    <TheProjectAnim />
+    <article class="margins mt-[25px]">
+        <ProjectContent :key="ProjectInfo.id" v-bind="{ ...ProjectInfo, projects: [] }" />
+    </article>
+    <section class="margins"><div class="flex items-center w-full gap-4 mt-12 mb-8">
+        <h3 class="text-xl font-light">See more projects</h3>
+        <div class="flex-1 border-b border-lightwhite"></div>
+    </div>
+    <ul class="flex-wrap justify-around -mb-16 md:gap-5 lg:gap-20 md:mb-0 md:flex">
+                <li v-for="projects in ProjectsListeSorted.slice(0, 3)" :key="projects.id">
+                    <RouterLink :to="{ name: 'projects-id', params: { id: projects.id } }" class="flex flex-col h-full">
+                        <CardProject :key="projects.id" v-bind="{ ...projects }" />
+                    </RouterLink>
+                </li>
+            </ul></section>
 </template>
 
 
