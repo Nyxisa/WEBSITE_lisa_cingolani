e42679a3942d3ea28ac7863f317224f3911a67f8
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-12-06 15:22:40 -0500

    svg update

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -18,8 +18,8 @@ console.log(ProjectsListeSorted)
         <div class="text-center lg:hidden">
             <h1 class="w-full mx-auto mb-8 bg-black border-none btn-style">PROJECTS</h1>
         </div>
-        <ul class="flex-wrap justify-around -mb-16 md:gap-5 lg:gap-20 md:mb-0 md:flex">
-            <li v-for="projects in ProjectsListeSorted" :key="projects.id">
+        <ul class="flex-wrap justify-around -mb-16 md:mb-0 md:flex">
+            <li v-for="projects in ProjectsListeSorted" :key="projects.id" class="xl:w-[30%]">
                 <RouterLink :to="{ name: 'projects-id', params: { id: projects.id } }" class="flex flex-col h-full">
                     <CardProject :key="projects.id" v-bind="{ ...projects }" />
                 </RouterLink>
