f50084258e4c66be1458dee0e15d59261fd81395
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2024-02-21 12:52:34 +0100

    gallery css fix

--- src/components/ProjectContent_en.vue
+++ src/components/ProjectContent_en.vue
@@ -40,21 +40,25 @@ const isVideo = (url: string): boolean => {
             <h1 class="mb-0 font-bold font-itc">{{ ProjectInfo.title }}</h1>
             <div class="w-0 h-[1px] anim-slide-in rounded-full mb-1 bg-lightwhite"></div>
             <h2 class="my-6 text-xl font-thin font-itc"> {{ ProjectInfo.category.join(" | ") }}</h2>
-            <img :src="urlMainImg" class="w-full h-auto pb-10 mx-auto rounded-simple" />
+            <img :src="urlMainImg" class="w-full h-auto mx-auto mb-10 rounded-simple" />
             <div class="intro-outro" v-html="ProjectInfo.intro"></div>
         </section>
 
-        <section class="flex flex-col justify-between lg:flex-row col-span-full">
-            <div class="mb-10 grid grid-cols-[1fr,_1fr] grid-rows-6 gap-4 h-fit [&_img]:rounded-simple">
-                <img v-if="urlAllImg[6]" :src="urlAllImg[6]"
-                    class="object-cover object-left h-full row-span-3 border border-lightwhite" alt="Image 1">
-                <img v-if="urlAllImg[1]" :src="urlAllImg[1]"
-                    class="object-cover object-left h-full row-span-4 border border-lightwhite" alt="Image 2">
-                <img v-if="urlAllImg[2]" :src="urlAllImg[2]"
-                    class="object-cover object-left h-full row-span-3 border border-lightwhite" alt="Image 3">
-                <img v-if="urlAllImg[5]" :src="urlAllImg[5]"
-                    class="object-cover object-left h-full row-span-2 border border-lightwhite" alt="Image 4">
+        <section class="flex flex-col justify-between xl:flex-row col-span-full">
+            <div class="flex flex-wrap justify-center w-full gap-4 md:flex-nowrap">
+                <div class="w-full sm:w-1/2 mb-1 md:mb-0 lg:w-1/3 [&_img]:w-full [&_img]:rounded-simple [&_img]:shadow-1">
+                    <img v-if="urlAllImg[6]" :src="urlAllImg[6]" alt="Image 1">
+                    <img v-if="urlAllImg[5]" :src="urlAllImg[5]" alt="Image 2" class="mt-8">
+                    <img v-if="urlAllImg[4]" :src="urlAllImg[4]" alt="Image 3" class="mt-8">
+                </div>
+                <div class="w-full sm:w-1/2 lg:w-1/3 [&_img]:w-full [&_img]:rounded-simple [&_img]:shadow-1">
+                    <img v-if="urlAllImg[3]" :src="urlAllImg[3]" alt="Image 4">
+                    <img v-if="urlAllImg[2]" :src="urlAllImg[2]" alt="Image 5" class="mt-8">
+                    <img v-if="urlAllImg[1]" :src="urlAllImg[6]" alt="Image 6" class="mt-8">
+                </div>
             </div>
+
+
             <div class="flex flex-col gap-10">
                 <div v-if="ProjectInfo.text_1" v-html="ProjectInfo.text_1"></div>
                 <div v-if="ProjectInfo.text_2" v-html="ProjectInfo.text_2"></div>
