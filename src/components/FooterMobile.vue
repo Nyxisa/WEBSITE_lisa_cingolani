a9e4be593211962e4857af0e307c63edf861707c
Nyxisa <lisa.cingolani@edu.univ-fcomte.fr>
2024-03-12 00:55:05 +0100

    newsletter form added

--- src/components/FooterMobile.vue
+++ src/components/FooterMobile.vue
@@ -5,7 +5,54 @@ import instagramLogo from './icons/instagram-logo.vue';
 import dribbbleLogo from './icons/dribbble-logo.vue';
 import behanceLogo from './icons/behance-logo.vue';
 
+import { ref } from 'vue';
+
+const email = ref('');
+const error = ref('');
+const submitted = ref(false);
+
+const resetStates = () => {
+    submitted.value = false;
+    error.value = '';
+};
+
+const resetForm = () => {
+    email.value = '';
+};
+
+const onSubmit = async () => {
+    resetStates();
+
+    try {
+        const response = await fetch("https://formcarry.com/s/xcH-sD-LvT3", {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: email.value })
+        });
+
+        const data = await response.json();
+
+        if (data.code === 200) {
+            submitted.value = true;
+            resetForm();
+        } else if (data.code === 422) {
+            error.value = data.message;
+        } else {
+            error.value = data.message;
+        }
+    } catch (err: any) {
+        error.value = err.message ? err.message : err;
+    }
+};
+
+const showNotification = () => submitted.value || error.value;
+const errorMessage = () => error.value ? error.value : '';
+
 </script>
+
 <template>
     <footer class="w-screen bg-gradient-to-t from-lightblack to-black footer">
         <div class="p-10">
@@ -38,13 +85,23 @@ import behanceLogo from './icons/behance-logo.vue';
 
             <div class="mt-20 w-[80%] mx-auto">
                 <h5 class="mb-10 text-center">{{ $t('footer.cta2') }}</h5>
-                <form action="#">
-                    <input type="email" id="email"
+                <form @submit.prevent="onSubmit">
+                    <input type="email" v-model="email" id="email"
                         class="block text-center w-full px-1 text-base bg-transparent border-b max-w-[300px] mx-auto border-b-lightwhite focus:outline-none text-lightwhite"
                         :placeholder="$t('footer.placeholder')">
-                </form>
-                <button type="submit" class="mx-auto mt-5 btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{
+
+                    <button v-if="!showNotification() || error" type="submit"
+                        class="mx-auto mt-5 btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{
                     $t('footer.btn') }}</button>
+                    <button v-if="!error && showNotification()"
+                        class=" mx-auto mt-5 bg-lightpurple btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{
+                    $t('footer.btn-ok') }}</button>
+                    <div v-else-if="error && showNotification()"
+                        class="p-5 mx-auto my-5 text-lightblack bg-lightorange rounded-simple w-fill">
+                        {{ error }}
+                    </div>
+
+                </form>
             </div>
             <img src="./icons/nav/logo.svg" class="w-[40%] h-auto mx-auto mt-20 fill-lightwhite max-w-[200px]" />
         </div>
--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -4,8 +4,54 @@ import linkedinLogo from './icons/linkedin-logo.vue';
 import instagramLogo from './icons/instagram-logo.vue';
 import dribbbleLogo from './icons/dribbble-logo.vue';
 import behanceLogo from './icons/behance-logo.vue';
+import { ref } from 'vue';
+
+const email = ref('');
+const error = ref('');
+const submitted = ref(false);
+
+const resetStates = () => {
+    submitted.value = false;
+    error.value = '';
+};
+
+const resetForm = () => {
+    email.value = '';
+};
+
+const onSubmit = async () => {
+    resetStates();
+
+    try {
+        const response = await fetch("https://formcarry.com/s/xcH-sD-LvT3", {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: email.value })
+        });
+
+        const data = await response.json();
+
+        if (data.code === 200) {
+            submitted.value = true;
+            resetForm();
+        } else if (data.code === 422) {
+            error.value = data.message;
+        } else {
+            error.value = data.message;
+        }
+    } catch (err: any) {
+        error.value = err.message ? err.message : err;
+    }
+};
+
+const showNotification = () => submitted.value || error.value;
+const errorMessage = () => error.value ? error.value : '';
 
 </script>
+
 <template>
     <footer class="footer w-[100vw] pt-20 bg-gradient-to-b from-lightblack to-black">
         <div class="flex justify-around gap-10 pb-5 mx-[6vw]">
@@ -33,16 +79,26 @@ import behanceLogo from './icons/behance-logo.vue';
 
             <div class="w-[300px]">
                 <h5 class="mb-2">{{ $t('footer.cta2') }}</h5>
-                <form action="#" class="py-4">
+                <form @submit.prevent="onSubmit" class="py-4">
                     <div>
                         <label for="email" class="block mb-2 text-sm">{{ $t('footer.mail') }}</label>
-                        <input type="email" id="email"
+                        <input type="email" v-model="email" id="email"
                             class="block w-full px-1 text-base bg-transparent border-b border-b-lightwhite focus:outline-none text-lightwhite"
                             :placeholder="$t('footer.placeholder')">
                     </div>
-                </form>
-                <button type="submit" class="my-4 btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{ $t('footer.btn')
+                    <button v-if="!showNotification() || error" type="submit"
+                        class="my-4 btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{
+                    $t('footer.btn')
                 }}</button>
+                    <button v-if="!error && showNotification()"
+                        class="my-4 btn focus:translate-y-[1px] hover:-translate-y-[1px]">{{
+                    $t('footer.btn-ok')
+                }}</button>
+                    <div v-else-if="error && showNotification()"
+                        class="p-5 mx-auto mb-5 text-lightblack bg-lightorange rounded-simple w-fill">
+                        {{ error }}
+                    </div>
+                </form>
             </div>
         </div>
         <span class="block py-4 pt-6 text-sm font-thin text-center">
